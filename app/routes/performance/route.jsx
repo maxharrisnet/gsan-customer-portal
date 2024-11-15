85de1519f4f50f5acabe1cb9677a9a48960f9127
Max Harris <hello@maxharris.net>
2024-11-15 01:23:45 -0800

    adding data to reports

--- app/routes/performance/route.jsx
+++ app/routes/performance/route.jsx
@@ -7,7 +7,6 @@ export const links = () => [{ rel: 'stylesheet', href: performanceStyles }];
 export { loader };
 
 export function getLatencyClass(latency) {
-	// Returns the class based on latency value
 	if (latency < 50) return 'latency-green';
 	else if (latency < 150) return 'latency-orange';
 	else return 'latency-red';
--- app/routes/reports.$provider.usage.jsx
+++ app/routes/reports.$provider.usage.jsx
@@ -6,9 +6,23 @@ import Layout from './../components/layout/Layout';
 export { loader };
 
 const Reports = () => {
-	const { provider } = useParams();
 	const { services } = useLoaderData();
+	console.log('📊 📊 📊 Data: ', services[0].modems[0].details.usage);
 	const [WebDataRocks, setWebDataRocks] = useState(null);
+
+	const flattenedData = services.flatMap((service) =>
+		service.modems.flatMap((modem) => {
+			const usage = Array.isArray(modem.details.usage) ? modem.details.usage : [modem.details.usage]; // Ensure usage is an array
+
+			return usage.map((entry) => ({
+				serviceName: service.name,
+				serviceId: service.id,
+				modemId: modem.id,
+				date: entry.date,
+				priority: entry.priority,
+			}));
+		})
+	);
 
 	useEffect(() => {
 		import('@webdatarocks/react-webdatarocks')
@@ -28,21 +42,26 @@ const Reports = () => {
 		<Layout>
 			<div>
 				<h1>Reports</h1>
-				<WebDataRocks
-					toolbar={true}
-					width='100%'
-					height='600px'
-					report={{
-						dataSource: {
-							data: [
-								{ Category: 'Beverages', Sales: 100 },
-								{ Category: 'Beverages', Sales: 200 },
-								{ Category: 'Snacks', Sales: 150 },
-								{ Category: 'Snacks', Sales: 250 },
-							],
-						},
-					}}
-				/>
+				<section className='section'>
+					<WebDataRocks
+						toolbar={true}
+						report={{
+							dataSource: {
+								data: flattenedData,
+							},
+							slice: {
+								rows: [{ uniqueName: 'serviceName' }, { uniqueName: 'modemId' }, { uniqueName: 'date' }],
+								columns: [{ uniqueName: 'Measures' }],
+								measures: [
+									{
+										uniqueName: 'priority',
+										aggregation: 'average',
+									},
+								],
+							},
+						}}
+					/>
+				</section>
 			</div>
 		</Layout>
 	);
