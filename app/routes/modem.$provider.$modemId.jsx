91533c1032844739442007e78c2b748aaff3c3b7
Max Harris <hello@maxharris.net>
2024-11-21 08:58:03 -0800

    damn

--- app/routes/modem.$provider.$modemId.jsx
+++ app/routes/modem.$provider.$modemId.jsx
@@ -139,26 +139,8 @@ export default function ModemDetails() {
 			<Sidebar>
 				<div>
 					<h2>{modem.name}</h2>
-					<a
-						href='/service-status'
-						class='px-3'
-					>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='16'
-							height='16'
-							fill='black'
-							class='bi bi-arrow-left'
-							viewBox='0 0 16 16'
-						>
-							<path
-								fill-rule='evenodd'
-								d='M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z'
-							/>
-						</svg>
-					</a>
 				</div>
-				<p>{modem.type}</p>
+				<p className='capitalize'>{modem.type}</p>
 				<p>{modem.id}</p>
 			</Sidebar>
 			<main className='content'>
